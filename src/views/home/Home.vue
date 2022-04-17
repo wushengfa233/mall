ef65aed1f5d0c713abd7050b163fdfb9fe4d244e
coolwu <wushengfa233@gmail.com>
2022-04-18 00:27:32 +0800

    首页增加了网络请求

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,40 @@
 <template>
-<div>
-  <h2>首页</h2>
+<div id="home">
+  <NavBar class="home-nav"> <div slot="center">购物车</div></NavBar>
 </div>
 </template>
 
 <script>
+import NavBar from "@/components/commom/navbar/NavBar";
+import {getHomeMultidata} from "@/network/home";
+
 export default {
-  name: "Home"
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+  components:{
+    NavBar
+  },
+  data(){
+    return{
+      banners:[],
+      recommends:[]
+    }
+  },
+  created() {
+  //  1.请求多个数据
+    getHomeMultidata().then(res=>{
+      console.log(res)
+      // this.result=res
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color: #ef4562;
+  color: #FFFFFF;
+}
 </style>
